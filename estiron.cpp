61ffe913aa43b902a2157cca51ee985d7d1c0efa
Maikals <miquicm@gmail.com>
2014-05-04 17:53:37 +0200

    estiron fet

--- estiron.cpp
+++ estiron.cpp
@@ -1,7 +1,25 @@
 #include "utils.PRO2"
 #include "Arbre.hpp"
 
-int max;
+
+void estiron (Arbre<int>& a, int& max) {
+	int x = a.arrel();
+	Arbre <int> a1, a2;
+	a.fills(a1, a2);
+	if (a1.es_buit() and a2.es_buit()) {
+		Arbre <int> a11, a12, a21, a22;
+		++max;
+		a1.plantar (max, a11, a22);
+		++max;
+		a2.plantar (max, a21, a22);
+		a.plantar (x, a1, a2);
+	}
+	else {
+		if (not a1.es_buit()) estiron(a1, max);
+		if (not a2.es_buit()) estiron(a2, max);
+		a.plantar(x, a1, a2);
+	} 
+}
 
 void escriure_arbre (Arbre<int>& a)
 {
@@ -16,16 +34,22 @@ void escriure_arbre (Arbre<int>& a)
 	}
 }
 
-void llegir_arbre(Arbre <int>& a) {
+void llegir_arbre(Arbre <int>& a, int& max) {
 	int x = readint();
 	if (x != -1) {
+		if (x > max) max = x;
 		Arbre <int> a1, a2;
-		llegir_arbre(a1);
-		llegir_arbre(a2);
+		llegir_arbre(a1, max);
+		llegir_arbre(a2, max);
 		a.plantar(x, a1, a2);
 	}
 }
 
 int main() {
+	int max = -1;
 	Arbre<int> a;
+	llegir_arbre (a, max);
+	estiron(a, max);
+	escriure_arbre(a);
+	cout << endl;
 }
