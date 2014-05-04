0f5ee2fa74d8862c312ca240dd67d6fabb78ae6d
Maikals <miquicm@gmail.com>
2014-05-04 17:24:58 +0200

    files init

---
+++ recorte.cpp
@@ -0,0 +1,22 @@
+#include "utils.PRO2"
+#include "Arbre.hpp"
+
+void recorte () 
+{
+	
+}
+
+void llegir_arbre(Arbre <int>& a) {
+	int x = readint();
+	if (x != -1) {
+		Arbre <int> a1, a2;
+		llegir_arbre(a1, a2);
+		a.plantar(x, a1, a2);
+	}
+}
+
+int main() {
+	Arbre<int> a;
+	llegir_arbre(a);
+
+}
