e7faab94afdd7d1b268f0dfa4d95c336c379905c
Maikals <miquicm@gmail.com>
2014-05-04 17:28:46 +0200

    files init2

--- emparejamiento.cpp
+++ emparejamiento.cpp
@@ -1,11 +1,25 @@
 #include "utils.PRO2"
 #include "Arbre.hpp"
 
+void escriure_arbre (Arbre<int>& a)
+{
+	if (not a.es_buit) {
+		int x = a.arrel();
+		cout << x << " ";
+		Arbre <int> a1, a2;
+		a.fills(a1, a2);
+		escriure_arbre (a1);
+		escriure_arbre (a2);
+		a.plantar (x, a1, a2);
+	}
+}
+
 void llegir_arbre(Arbre <int>& a) {
 	int x = readint();
 	if (x != -1) {
 		Arbre <int> a1, a2;
-		llegir_arbre(a1, a2);
+		llegir_arbre(a1);
+		llegir_arbre(a2);
 		a.plantar(x, a1, a2);
 	}
 }
--- recorte.cpp
+++ recorte.cpp
@@ -3,20 +3,34 @@
 
 void recorte () 
 {
-	
+
 }
 
 void llegir_arbre(Arbre <int>& a) {
 	int x = readint();
 	if (x != -1) {
 		Arbre <int> a1, a2;
-		llegir_arbre(a1, a2);
+		llegir_arbre(a1);
+		llegir_arbre(a2);
 		a.plantar(x, a1, a2);
+	}
+}
+
+void escriure_arbre (Arbre<int>& a)
+{
+	if (not a.es_buit) {
+		int x = a.arrel();
+		cout << x << " ";
+		Arbre <int> a1, a2;
+		a.fills(a1, a2);
+		escriure_arbre (a1);
+		escriure_arbre (a2);
+		a.plantar (x, a1, a2);
 	}
 }
 
 int main() {
 	Arbre<int> a;
 	llegir_arbre(a);
-
+	escriure_arbre(a);
 }
